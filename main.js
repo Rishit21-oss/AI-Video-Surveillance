02bdc83f58d2601b30a5a769345777e580293e42
Rishit21-oss <81700717+Rishit21-oss@users.noreply.github.com>
2022-06-10 20:00:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 status = "";
 video="";
-
+objects= [];
 function preload(){
     video = createVideo('video.mp4');
     video.hide();
@@ -13,6 +13,20 @@ function setup(){
 
 function draw(){
     image(video, 0, 0,480, 380);
+    if(status != " "){
+        objectDetector.detect(video, gotResult);
+        for(i=0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = "Status : Objects Detected ";
+            document.getElementById("number_of_objects").innerHTML = "Number of objects :"+ objects.length;
+
+            fill("#FF0000");
+            confidence = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + confidence + "%" , objects[i].x + 15 , objects[i].y + 15);
+            noFill()
+            stroke("#FF0000");
+            rect(objects[i].x , objects[i].y , objects[i].width , objects[i].height);
+        }
+    }
 }
 
 function start(){
@@ -26,4 +40,12 @@ function modelLoaded(){
     video.loop();
     video.speed(1);
     video.volume(0);
+}
+
+function gotResult(error , results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
 }
